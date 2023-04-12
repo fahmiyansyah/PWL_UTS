d225758707a34bf4b6f13db3446083a30d36a212
Septian Fahmi Ardiansyah <fahrisepti888@gmail.com>
2023-04-12 19:40:33 +0700

    Connect Database

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\BarangController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,4 +16,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/warga',[BarangController::class]);
+Route::get('/barang',[BarangController::class, 'index']);
